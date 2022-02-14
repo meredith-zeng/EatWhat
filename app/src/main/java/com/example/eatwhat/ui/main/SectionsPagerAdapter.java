a62e971bd09eca3dac43adf1f07d47b8c0ebfa92
Yulin Zeng <1191220341@qq.com>
2022-02-14 14:38:15 -0800

    AppBarLayout

--- app/src/main/java/com/example/eatwhat/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/eatwhat/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.eatwhat.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -41,6 +41,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return 3;
     }
 }
