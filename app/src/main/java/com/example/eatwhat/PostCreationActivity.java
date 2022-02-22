8059ded0d79432a667b9b86e3d9f9bb970d5208b
DanielLu127 <lxinwei7@vt.edu>
2022-02-21 16:05:15 -0800

    Version 0221 1. added change password 2. post creation back button

--- app/src/main/java/com/example/eatwhat/PostCreationActivity.java
+++ app/src/main/java/com/example/eatwhat/PostCreationActivity.java
@@ -3,12 +3,27 @@ package com.example.eatwhat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostCreationActivity extends AppCompatActivity {
 
+    private ImageButton mCancelBtn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_creation);
+        cancel_button_init();
+    }
+
+    private void cancel_button_init() {
+        mCancelBtn = findViewById(R.id.post_creation_cancel_btn);
+        mCancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
