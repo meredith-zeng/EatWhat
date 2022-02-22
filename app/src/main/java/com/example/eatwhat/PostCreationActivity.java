7e1fe08a489828d92054dd296ebe72760f48f543
Yulin Zeng <37796757+meredith-zeng@users.noreply.github.com>
2022-02-22 15:31:57 -0800

    RetrofitClient merge preparation

--- app/src/main/java/com/example/eatwhat/PostCreationActivity.java
+++ app/src/main/java/com/example/eatwhat/PostCreationActivity.java
@@ -21,48 +21,11 @@ public class PostCreationActivity extends AppCompatActivity {
 
     private ImageButton mCancelBtn;
 
-
-    private Button getData;
-    private final static String TAG = "http get test";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_creation);
         cancel_button_init();
-
-
-        getData = findViewById(R.id.getData);
-        getData.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                RestaurantService methods = RetrofitClient.getRetrofitInstance().create(RestaurantService.class);
-
-                String location = "Santa Clara University";
-                String restaurantId = "WavvLdfdP6g8aZTtbBQHTw";
-//                Call<Restaurant> call = methods.getRestaurantByLocation(location, null, null, null);
-                Call<Business> call = methods.getRestaurantById(restaurantId);
-                call.enqueue(new Callback<Business>() {
-                    @Override
-                    public void onResponse(Call<Business> call, Response<Business> response) {
-                        Log.e(TAG, "onResponse: code" + response.code());
-//                        for(Business res: response.body().getBusinesses() ){
-//                            Log.e(TAG, res.getName());
-//                        }
-                        Log.e(TAG, response.body().getName());
-                    }
-
-                    @Override
-                    public void onFailure(Call<Business> call, Throwable t) {
-
-                    }
-                });
-
-            }
-        });
-
     }
 
     private void cancel_button_init() {
