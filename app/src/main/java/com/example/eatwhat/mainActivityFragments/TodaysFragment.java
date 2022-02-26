bd35e25662ef94ebb6d525937ddabc94540588fa
Yulin Zeng <37796757+meredith-zeng@users.noreply.github.com>
2022-02-25 20:16:54 -0800

    random recommendation

--- app/src/main/java/com/example/eatwhat/mainActivityFragments/TodaysFragment.java
+++ app/src/main/java/com/example/eatwhat/mainActivityFragments/TodaysFragment.java
@@ -11,6 +11,7 @@ import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -27,19 +28,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
 import com.example.eatwhat.R;
+import com.example.eatwhat.activity.restaurant.RestaurantPageActivity;
 import com.example.eatwhat.cardview.RestaurantCard;
 import com.example.eatwhat.service.RestaurantService;
 import com.example.eatwhat.service.RetrofitClient;
 import com.example.eatwhat.service.pojo.Business;
 import com.example.eatwhat.service.pojo.Restaurant;
+import com.example.eatwhat.util.RandomUtil;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -54,6 +65,15 @@ public class TodaysFragment extends Fragment {
     private ImageView imgHand;
     private ObjectAnimator anim;
 
+    private ImageView iv_image;
+    private TextView tvTitle;
+
+    private RestaurantCard restaurantCard;
+    private String restaurantId;
+
+    private Button bt_today;
+    private static final String TOKEN = "RO1Oxxrhr0ZE2nvxEvJ0ViejBTWKcLLhPQ7wg6GGPlGiHvjwaLPU2eWlt4myH3BC1CP4RSzIQ7UCFjZ-FBaF_4ToUYHfs6FF6FwipyMuz47xVvlpEr6gDv-2YRQUYnYx";
+
     public TodaysFragment() {
         // Required empty public constructor
     }
@@ -66,6 +86,8 @@ public class TodaysFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_todays, container, false);
 
         imgHand = (ImageView) view.findViewById(R.id.imgHand);
+
+
         sensorManager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
         shakeListener = new ShakeSensorListener();
 
@@ -111,10 +133,38 @@ public class TodaysFragment extends Fragment {
                 isShake = true;
                 playSound(getContext());
                 vibrate( 500);
+
+                RetrofitClient retrofitClient = new RetrofitClient();
+                RestaurantService methods = retrofitClient.getRetrofit().create(RestaurantService.class);
+
+                String location = "Santa Clara";
+                int offset = RandomUtil.getRandomNumber(0, 100);
+                Call<Restaurant> call = methods.queryRestaurantByLocation(location, 1, offset);
+                Log.d(TAG, "run: " + call.toString());
+                call.enqueue(new Callback<Restaurant>() {
+                    @Override
+                    public void onResponse(Call<Restaurant> call, Response<Restaurant> response) {
+                        if (response.code() == 200){
+                            Business business = response.body().getBusinesses().get(0);
+                            restaurantCard = new RestaurantCard(business.getImageUrl(), business.getName(), business.getCategories().get(0).toString(), false);
+                            restaurantId = business.getId();
+                            Log.e("Shake res" , restaurantCard.getRestaurantImageUrl());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Restaurant> call, Throwable t) {
+                        Log.e("Shake res" ,t.toString());
+                    }
+                });
+
+
                 //仿网络延迟操作，这里可以去请求服务器...
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
+
+
                         //弹框
                         showDialog();
                         //动画取消
@@ -143,10 +193,37 @@ public class TodaysFragment extends Fragment {
         final AlertDialog mAlertDialog = new AlertDialog.Builder(getContext()).show();
         View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_dialog,null);
         mAlertDialog.setContentView(view);
+
+        iv_image = (ImageView) view.findViewById(R.id.shake_image);
+        tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+        bt_today = (Button) view.findViewById(R.id.bt_today);
+
+
+
+        tvTitle.setText(restaurantCard.getTitle());
+
+        GlideUrl glideUrl = new GlideUrl(restaurantCard.getRestaurantImageUrl(), new LazyHeaders.Builder()
+                .addHeader("Authorization", " Bearer " + TOKEN)
+                .build());
+        Glide.with(getContext())
+                .load(glideUrl)
+                .into(iv_image);
+
+        bt_today.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), RestaurantPageActivity.class);
+                intent.putExtra("title", restaurantCard.getTitle());
+                intent.putExtra("content", restaurantCard.getContent());
+                intent.putExtra("imageUrl", restaurantCard.getRestaurantImageUrl());
+                intent.putExtra("id", restaurantId);
+                getContext().startActivity(intent);
+            }
+        });
+
         mAlertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialog) {
-                //这里让弹框取消后，才可以执行下一次的摇一摇
                 isShake = false;
                 mAlertDialog.cancel();
             }
@@ -155,26 +232,6 @@ public class TodaysFragment extends Fragment {
         window.setBackgroundDrawable(new ColorDrawable(0x00000000));
     }
 
-//    private void initData(){
-//        RetrofitClient retrofitClient = new RetrofitClient();
-//        RestaurantService methods = retrofitClient.getRetrofit().create(RestaurantService.class);
-//
-//        String location = "Santa Clara University";
-//        Call<Restaurant> call = methods.queryRestaurantByLocation(location, 1, 1);
-//        call.enqueue(new Callback<Restaurant>() {
-//            @Override
-//            public void onResponse(Call<Restaurant> call, Response<Restaurant> response) {
-//                if (response.code() == 200){
-//                    Business business = response.body().getBusinesses().get(0);
-//                    RestaurantCard restaurantCard = new RestaurantCard(business.getImageUrl(), business.getName(), business.getCategories().toString(), false);
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Restaurant> call, Throwable t) {
-//
-//            }
-//        });
-//    }
+
 
 }
