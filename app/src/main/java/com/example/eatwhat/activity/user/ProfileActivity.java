7b717574bdb3cad35dfc3ec478fcee8bbc94637d
Ce Pang <pcdota123@gmail.com>
2022-03-01 00:20:44 -0800

    Post Creation Version 4

--- app/src/main/java/com/example/eatwhat/activity/user/ProfileActivity.java
+++ app/src/main/java/com/example/eatwhat/activity/user/ProfileActivity.java
@@ -8,14 +8,21 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import com.example.eatwhat.activity.user.ChangePasswordActivity;
-import com.example.eatwhat.activity.MainActivity;
+import android.widget.TextView;
+
 import com.example.eatwhat.R;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private Button change;
     private Intent homeIntent;
+    ShapeableImageView imageView;
+    TextView email_addr,username;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,14 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
         setContentView(R.layout.activity_profile);
         //set back button
         setToolBar();
+
+        imageView = findViewById(R.id.img_launcher_icon);
+        email_addr = findViewById(R.id.email_address_in_profile);
+        username = findViewById(R.id.username_in_profile);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String uid = user.getUid();
+
 
         change = (Button) findViewById(R.id.changepassword);
         change.setOnClickListener(new View.OnClickListener() {
