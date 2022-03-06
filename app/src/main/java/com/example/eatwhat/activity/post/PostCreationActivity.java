e85cee3bdd50e8499a5841e2988018d6efcaa14e
Ce Pang <pcdota123@gmail.com>
2022-03-05 20:45:38 -0800

    post detail version 1

--- app/src/main/java/com/example/eatwhat/activity/post/PostCreationActivity.java
+++ app/src/main/java/com/example/eatwhat/activity/post/PostCreationActivity.java
@@ -145,7 +145,9 @@ public class PostCreationActivity extends AppCompatActivity {
                     image_url = "gs://project-43404.appspot.com/postImages/" + postId + "/" + postId + ".jpeg";
                     List<String> likedList = new ArrayList<>();
                     likedList.add(uid);
-                    PostCard postCard = new PostCard(uid, postId, title_str, comment_str, 0, image_url,name_str, ratings, likedList);
+
+                    PostCard postCard = new PostCard(uid, postId, title_str, comment_str, 0, image_url,name_str, ratings);
+                    postCard.setLikedUidList(likedList);
 
 
                     mDatabase.child(postId).setValue(postCard)
